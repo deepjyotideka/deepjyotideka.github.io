9b8b670611a6bde55c6252efd5414c86a87c5752
deepjyotideka <deepjyotideka@gmail.com>
2025-02-01 03:55:26 +0000

    Deploying to gh-pages from @ deepjyotideka/deepjyotideka.github.io@6239dc2bcc8f968807e4978500d9d292411e1deb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -492,35 +492,28 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%64%65%65%70%6A%38%37@%6D%69%74.%65%64%75", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/deepjyoti-deka-8a44388", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=gv6Jmj0AAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://energy.mit.edu/profile/deepjyoti-deka/", "_blank");
         },
       },{
       id: 'light-theme',
