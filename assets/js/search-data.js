ecb1b24cd1baabcd4b604b2c5bec9a6dec5c685d
deepjyotideka <deepjyotideka@gmail.com>
2025-02-01 22:36:20 +0000

    Deploying to gh-pages from @ deepjyotideka/deepjyotideka.github.io@d3efe5cf1dba44ec99cb1ba1afc431ab9234695d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -389,12 +389,7 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-point-right-open-post-doctoral-position-in-flexibility-modeling-of-data-center-loads",
+    },{id: "news-point-right-open-post-doctoral-position-in-flexibility-modeling-of-data-center-loads",
           title: ':point_right: Open Post-doctoral position in flexibility modeling of data-center loads',
           description: "",
           section: "News",},{id: "projects-project-1",
