2e66c0a983386739d0b2a05557db46238bc532b2
deepjyotideka <deepjyotideka@gmail.com>
2025-02-01 18:15:07 +0000

    Deploying to gh-pages from @ deepjyotideka/deepjyotideka.github.io@98bbd947239ab5d5e52bf40183257146732d5314 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "Current Research Projects",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
@@ -36,13 +36,6 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
-          },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
           },
         },{id: "nav-collaborators",
           title: "Collaborators",
